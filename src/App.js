bf711f27656965c1c879bcac74f8279679d86d20
Felipe Pereira <pereira_felipesilva@outlook.com>
2019-08-02 11:30:06 -0300

    Criação do módulo Autor.js pra lidar com o envio do formulário e atualização da tabela

--- src/App.js
+++ src/App.js
@@ -1,80 +1,22 @@
 import React, { Component } from 'react';
 import './css/pure-min.css';
 import './css/side-menu.css';
-import $ from 'jquery';
-import InputForm from './Components/InputForm';
-import SubmitButton from './Components/SubmitButton';
+import AutorBox from './Autor';
 
 class App extends Component {
-  constructor(){
-
-    super();
-    this.state = {lista: [], nome:'', email: '', senha: ''};
-    this.enviaForm = this.enviaForm.bind(this);
-    this.setNome = this.setNome.bind(this);
-    this.setEmail = this.setEmail.bind(this);
-    this.setSenha = this.setSenha.bind(this);
-  }
-
-  componentDidMount(){
-
-    $.ajax({
-      method: "GET",
-      url: "http://cdc-react.herokuapp.com/api/autores",
-      dataType: "JSON",
-      success:function(resposta){
-
-        this.setState({lista: resposta})
-      }.bind(this),
-      error:()=>{console.log("Não foi possível recuperar os dados do servidor")}
-    })
-  }
-
-  enviaForm(event){
-
-    event.preventDefault();
-    $.ajax({
-
-      method: "POST",
-      url: "http://cdc-react.herokuapp.com/api/autores",
-      contentType: "application/json",
-      data: JSON.stringify({ nome: this.state.nome, email: this.state.email, senha: this.state.senha }),
-      dataType: "json",
-      success: function(resposta){
-        this.setState({ lista: resposta });
-        console.log(`Dados enviados com sucesso!`);
-      }.bind(this),
-      error: function(erro){
-        console.log(`Não foi possível enviar os dados para o servidor.`);
-        throw new Error(erro);
-      }
-    })
-  }
-
-  setNome(event){
-    this.setState({ nome: event.target.value });
-  }
-
-  setEmail(event){
-    this.setState({ email: event.target.value });
-  }
-
-  setSenha(event){
-    this.setState({ senha: event.target.value });
-  }
 
   render(){
 
     return (
       <div id="layout">
+
         <a href="#menu" id="menuLink" className="menu-link">
           <span></span>
         </a>
-  
         <div id="menu">
+          
           <div className="pure-menu">
             <a className="pure-menu-heading" href="#menu">Company</a>
-  
             <ul className="pure-menu-list">
               <li className="pure-menu-item"><a href="#menu" className="pure-menu-link">Home</a></li>
               <li className="pure-menu-item"><a href="#menu" className="pure-menu-link">Autor</a></li>
@@ -84,42 +26,12 @@ class App extends Component {
         </div>
   
         <div id="main">
+
           <div className="header">
             <h1>Cadastro de Autores</h1>
           </div>
           <div className="content" id="content">
-            <div className="pure-form pure-form-aligned">
-              <form className="pure-form pure-form-aligned" onSubmit={this.enviaForm}>
-                <InputForm label="Nome" id="nome" type="text" name="nome" value={this.state.nome} onChange={this.setNome} />
-                <InputForm label="Email" id="email" type="email" name="email" value={this.state.email} onChange={this.setEmail} />
-                <InputForm label="Senha" id="senha" type="password" name="senha" value={this.state.senha} onChange={this.setSenha} />
-                <SubmitButton label="Salvar" />
-              </form>
-  
-            </div>
-            <div>
-              <table className="pure-table">
-                <thead>
-                  <tr>
-                    <th>Nome</th>
-                    <th>email</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {
-                    this.state.lista.map(function(autor){
-                      return (
-
-                        <tr key={autor.id}>
-                          <td>{autor.nome}</td>
-                          <td>{autor.email}</td>
-                        </tr>
-                      )
-                    })
-                  }
-                </tbody>
-              </table> 
-            </div>
+            <AutorBox />
           </div>
         </div>
       </div>
---
+++ src/Autor.js
@@ -0,0 +1,138 @@
+import React, { Component } from 'react';
+import $ from 'jquery';
+import InputForm from './Components/InputForm';
+import SubmitButton from './Components/SubmitButton';
+
+class FormularioAutores extends Component {
+
+  constructor(){
+
+    super();
+    this.state = {nome:'', email: '', senha: ''};
+    this.enviaForm = this.enviaForm.bind(this);
+    this.setNome = this.setNome.bind(this);
+    this.setEmail = this.setEmail.bind(this);
+    this.setSenha = this.setSenha.bind(this);
+  }
+
+  render(){
+
+    return(
+
+      <div className="pure-form pure-form-aligned">
+        <form className="pure-form pure-form-aligned" onSubmit={this.enviaForm}>
+          <InputForm label="Nome" id="nome" type="text" name="nome" value={this.state.nome} onChange={this.setNome} />
+          <InputForm label="Email" id="email" type="email" name="email" value={this.state.email} onChange={this.setEmail} />
+          <InputForm label="Senha" id="senha" type="password" name="senha" value={this.state.senha} onChange={this.setSenha} />
+          <SubmitButton label="Salvar" />
+        </form>
+      </div>
+    );
+  }
+
+  enviaForm(event){
+
+    event.preventDefault();
+    $.ajax({
+
+      method: "POST",
+      url: "http://cdc-react.herokuapp.com/api/autores",
+      contentType: "application/json",
+      data: JSON.stringify({ nome: this.state.nome, email: this.state.email, senha: this.state.senha }),
+      dataType: "json",
+      success: (resposta)=> {
+        this.props.callbackAtualiza(resposta);
+        console.log(`Dados enviados com sucesso!`);
+      },
+      error: erro => {
+        console.log(`Não foi possível enviar os dados para o servidor.`);
+        throw new Error(erro);
+      }
+    })
+  }
+
+  setNome(event){
+    this.setState({ nome: event.target.value });
+  }
+
+  setEmail(event){
+    this.setState({ email: event.target.value });
+  }
+
+  setSenha(event){
+    this.setState({ senha: event.target.value });
+  }
+}
+
+class TabelaAutores extends Component {
+
+  render(){
+
+    return(
+
+      <div>
+        <table className="pure-table">
+          <thead>
+            <tr>
+              <th>Nome</th>
+              <th>email</th>
+            </tr>
+          </thead>
+          <tbody>
+            {
+              this.props.lista.map(function(autor){
+                return (
+
+                  <tr key={autor.id}>
+                    <td>{autor.nome}</td>
+                    <td>{autor.email}</td>
+                  </tr>
+                );
+              })
+            }
+          </tbody>
+        </table> 
+      </div>
+    );
+  }
+
+}
+
+export default class AutorBox extends Component {
+  
+  constructor(){
+    
+    super();
+    this.state = {lista: []};
+    this.atualizaListagem = this.atualizaListagem.bind(this);
+  }
+  
+  componentDidMount(){
+    
+    $.ajax({
+      method: "GET",
+      url: "http://cdc-react.herokuapp.com/api/autores",
+      dataType: "JSON",
+      success: function(resposta){
+        
+        this.setState({lista: resposta})
+      }.bind(this),
+      error: ()=> {console.log("Não foi possível recuperar os dados do servidor")}
+    })
+  }
+
+  atualizaListagem(novaLista){
+    
+    this.setState({lista: novaLista});
+  }
+
+  render(){
+    
+    return(
+      <>
+        <FormularioAutores callbackAtualiza={this.atualizaListagem} />
+        <TabelaAutores lista={this.state.lista} />
+      </>
+    );
+  }
+}
