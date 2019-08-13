af29483e22017e97b2745d877ba97488c896e11a
Felipe Pereira <pereira_felipesilva@outlook.com>
2019-08-13 13:15:21 -0300

    Implementação das rotas no projeto

--- src/Autor.js
+++ src/Autor.js
@@ -1,144 +1,139 @@
 import React, { Component } from 'react';
 import $ from 'jquery';
-import PubSub from 'pubsub-js';
 import InputForm from './Components/InputForm';
-import SubmitButton from './Components/SubmitButton';
-import ErrorHandler from './ErrorHandler';
+import PubSub from 'pubsub-js';
+import ErrorHandler from  './ErrorHandler';
 
-class FormularioAutores extends Component {
+class FormularioAutor extends Component {
 
-  constructor(){
-
-    super();
-    this.state = {nome:'', email: '', senha: ''};
+  constructor() {
+    super();    
+    this.state = {nome:'',email:'',senha:''};
     this.enviaForm = this.enviaForm.bind(this);
     this.setNome = this.setNome.bind(this);
     this.setEmail = this.setEmail.bind(this);
     this.setSenha = this.setSenha.bind(this);
   }
 
-  render(){
-
-    return(
-
-      <div className="pure-form pure-form-aligned">
-        <form className="pure-form pure-form-aligned" onSubmit={this.enviaForm}>
-          <InputForm label="Nome" id="nome" type="text" name="nome" value={this.state.nome} onChange={this.setNome} />
-          <InputForm label="Email" id="email" type="email" name="email" value={this.state.email} onChange={this.setEmail} />
-          <InputForm label="Senha" id="senha" type="password" name="senha" value={this.state.senha} onChange={this.setSenha} />
-          <SubmitButton label="Salvar" />
-        </form>
-      </div>
-    );
-  }
-
-  enviaForm(event){
-
-    event.preventDefault();
+  enviaForm(evento){
+    evento.preventDefault();    
     $.ajax({
-
-      method: "POST",
-      url: "http://cdc-react.herokuapp.com/api/autores",
-      contentType: "application/json",
-      data: JSON.stringify({ nome: this.state.nome, email: this.state.email, senha: this.state.senha }),
-      dataType: "json",
-      success: (novaListagem) => {
-        PubSub.publish('atualiza-lista-autores', novaListagem);
-        this.setState({ nome:'', email: '', senha: '' });
-        console.log(`Dados enviados com sucesso!`);
-      },
-      error: resposta => {
-        if(resposta.status === 400){
+      url:'http://cdc-react.herokuapp.com/api/autores',
+      contentType:'application/json',
+      dataType:'json',
+      type:'post',
+      data: JSON.stringify({nome:this.state.nome,email:this.state.email,senha:this.state.senha}),
+      success: function(novaListagem){
+        PubSub.publish('atualiza-lista-autores',novaListagem);        
+        this.setState({nome:'',email:'',senha:''});
+      }.bind(this),
+      error: function(resposta){
+        if(resposta.status === 400) {
           new ErrorHandler().displayError(resposta.responseJSON);
         }
       },
-      beforeSend: ()=> {
-
-        PubSub.publish('limpa-erros', {});
-      }
-    })
+      beforeSend: function(){
+        PubSub.publish("limpa-erros",{});
+      }      
+    });
   }
 
-  setNome(event){
-    this.setState({ nome: event.target.value });
+  setNome(evento){
+    this.setState({nome:evento.target.value});
   }
 
-  setEmail(event){
-    this.setState({ email: event.target.value });
-  }
+  setEmail(evento){
+    this.setState({email:evento.target.value});
+  }  
 
-  setSenha(event){
-    this.setState({ senha: event.target.value });
-  }
+  setSenha(evento){
+    this.setState({senha:evento.target.value});
+  }  
+
+	render() {
+		return (
+            <div className="pure-form pure-form-aligned">
+              <form className="pure-form pure-form-aligned" onSubmit={this.enviaForm} method="post">
+                <InputForm id="nome" type="text" name="nome" value={this.state.nome} onChange={this.setNome} label="Nome"/>                                              
+                <InputForm id="email" type="email" name="email" value={this.state.email} onChange={this.setEmail} label="Email"/>                                              
+                <InputForm id="senha" type="password" name="senha" value={this.state.senha} onChange={this.setSenha} label="Senha"/>                                                                      
+                <div className="pure-control-group">                                  
+                  <label></label> 
+                  <button type="submit" className="pure-button pure-button-primary">Gravar</button>                                    
+                </div>
+              </form>             
+
+            </div>  
+
+		);
+	}
 }
 
 class TabelaAutores extends Component {
 
-  render(){
-
-    return(
-
-      <div>
-        <table className="pure-table">
-          <thead>
-            <tr>
-              <th>Nome</th>
-              <th>email</th>
-            </tr>
-          </thead>
-          <tbody>
-            {
-              this.props.lista.map(function(autor){
-                return (
-
-                  <tr key={autor.id}>
-                    <td>{autor.nome}</td>
-                    <td>{autor.email}</td>
-                  </tr>
-                );
-              })
-            }
-          </tbody>
-        </table> 
-      </div>
-    );
-  }
-
+	render() {
+		return(
+                    <div>            
+                      <table className="pure-table">
+                        <thead>
+                          <tr>
+                            <th>Nome</th>
+                            <th>email</th>
+                          </tr>
+                        </thead>
+                        <tbody>
+                          {
+                            this.props.lista.map(function(autor){
+                              return (
+                                <tr key={autor.id}>
+                                  <td>{autor.nome}</td>
+                                  <td>{autor.email}</td>
+                                </tr>
+                              );
+                            })
+                          }
+                        </tbody>
+                      </table> 
+                    </div>             		
+		);
+	}
 }
 
 export default class AutorBox extends Component {
-  
-  constructor(){
-    
-    super();
-    this.state = {lista: []};
-  }
-  
-  componentDidMount(){
-    
-    $.ajax({
-      method: "GET",
-      url: "http://cdc-react.herokuapp.com/api/autores",
-      dataType: "JSON",
-      success: function(resposta){
-        
-        this.setState({lista: resposta})
-      }.bind(this),
-      error: ()=> {console.log("Não foi possível recuperar os dados do servidor")}
-    })
 
-    PubSub.subscribe('atualiza-lista-autores', function(topico,novaListagem){
-      this.setState({lista:novaListagem});
-    }.bind(this));
+  constructor() {
+    super();    
+    this.state = {lista : []};    
   }
+
+  componentDidMount(){  
+    $.ajax({
+        url:"http://cdc-react.herokuapp.com/api/autores",
+        dataType: 'json',
+        success:function(resposta){    
+          this.setState({lista:resposta});
+        }.bind(this)
+      } 
+    );          
+
+    PubSub.subscribe('atualiza-lista-autores',function(topico,novaLista){
+      this.setState({lista:novaLista});
+    }.bind(this));
+  }   
+
 
   render(){
-    
-    return(
-      <>
-        <FormularioAutores />
-        <TabelaAutores lista={this.state.lista} />
-      </>
+    return (
+      <div>
+        <div className="header">
+          <h1>Cadastro de autores</h1>
+        </div>
+        <div className="content" id="content">                            
+          <FormularioAutor/>
+          <TabelaAutores lista={this.state.lista}/>        
+        </div>      
+
+      </div>
     );
   }
 }
